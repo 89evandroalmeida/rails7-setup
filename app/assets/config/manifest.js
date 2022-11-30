9871500f2e9986c5b24c23f0cf2aa756a0d8e48f
Evandro Almeida <89evandroalmeida@gmail.com>
2022-11-29 23:22:32 -0200

    Tema SB Admin

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link fontawesome-free/css/all.min.css
+//= link jquery/jquery.min.js
+//= link bootstrap/js/bootstrap.bundle.min.js
+//= link jquery-easing/jquery.easing.min.js
+//= link_directory ../javascripts .js
