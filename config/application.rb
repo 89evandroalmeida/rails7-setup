a99cd1ef82a7a8f55ee4e128f1ef73a78dcf95c3
Evandro Almeida <89evandroalmeida@gmail.com>
2022-12-27 15:40:01 -0200

    Páginas amigáveis para os erros 404 e 500

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Rails7Setup
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.exceptions_app = self.routes
+
     config.i18n.default_locale = ENV["DEFAULT_LOCALE"].to_sym
   end
 end
